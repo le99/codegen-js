d4311bd809838c431bde2b9f496af400a25917ff
le <le@debian>
2020-12-30 12:12:36 -0500

    Test refactor with await

--- test/opsTest.js
+++ test/opsTest.js
@@ -9,22 +9,18 @@ cgConstructor.Handlebars.registerHelper('loud', function (aString) {
   return aString.toUpperCase()
 });
 
-beforeEach(()=>{
-  return fs.mkdir('./testDir', {recursive: true})
-    .then(()=>{
-      return fs.writeFile('./testDir/f1.txt', 'hi');
-    });
+beforeEach(async ()=>{
+  await fs.mkdir('./testDir', {recursive: true});
+  await  fs.writeFile('./testDir/f1.txt', 'hi');
 });
 
-afterEach(()=>{
-  return fs.rmdir('./testDir', {recursive: true})
-  .then(()=>{
-    return fs.rmdir('./outDir', {recursive: true})
-  });
+afterEach(async ()=>{
+  await fs.rmdir('./testDir', {recursive: true});
+  await fs.rmdir('./outDir', {recursive: true});
 });
 
 describe("bad initialization", ()=>{
-  it("bad inDir", ()=>{
+  it("bad inDir", async ()=>{
     try{
       cgConstructor({inDir: {}});
       assert.fail();
@@ -34,7 +30,7 @@ describe("bad initialization", ()=>{
     }
   });
   
-  it("bad outDir", ()=>{
+  it("bad outDir", async ()=>{
     try{
       cgConstructor({outDir: {}});
       assert.fail();
@@ -44,7 +40,7 @@ describe("bad initialization", ()=>{
     }
   });
   
-  it("bad linux", ()=>{
+  it("bad linux", async ()=>{
     try{
       cgConstructor({linux: "something"});
       assert.fail();
@@ -57,260 +53,176 @@ describe("bad initialization", ()=>{
 
 
 describe("copy", ()=>{
-  it("same folder", ()=>{
+  it("same folder", async ()=>{
 
     let f2 = './testDir/f2.txt';
 
-    return cg.copy('./testDir/f1.txt', f2)
-      .then(()=>{
-        return fs.stat(f2);
-      }).then(f =>{
-        assert.isTrue(f.isFile(), true);
-      });
+    await cg.copy('./testDir/f1.txt', f2)
+    let f = await fs.stat(f2);
+    assert.isTrue(f.isFile(), true);
   })
   
-  it("sub folder", ()=>{
+  it("sub folder", async ()=>{
 
     let f2 = './testDir/sub/f2.txt';
 
-    return cg.copy('./testDir/f1.txt', f2)
-      .then(()=>{
-        return fs.stat(f2);
-      }).then(f =>{
-        assert.isTrue(f.isFile(), true);
-      });
+    await cg.copy('./testDir/f1.txt', f2)
+    let f = await fs.stat(f2);
+    assert.isTrue(f.isFile(), true);
   })
 });
 
 
 describe("copyDir", ()=>{
-  it("same folder", ()=>{
+  it("same folder", async ()=>{
 
-    return fs.mkdir('./testDir/dir1')
-      .then(()=>{
-        return fs.writeFile('./testDir/dir1/t1.txt', 'hello');
-      })
-      .then(()=>{
-        return cg.copyDir('./testDir/dir1', './testDir/dir2')
-      })
-      .then(()=>{
-        return fs.stat('./testDir/dir2/t1.txt');
-      }).then(f =>{
-        assert.isTrue(f.isFile(), true);
-      });
+    await fs.mkdir('./testDir/dir1');
+    await fs.writeFile('./testDir/dir1/t1.txt', 'hello');
+    await cg.copyDir('./testDir/dir1', './testDir/dir2');
+    let f = await fs.stat('./testDir/dir2/t1.txt');
+    assert.isTrue(f.isFile(), true);
   })
-  
 });
 
 
 describe("compile", ()=>{
-  it("same folder", ()=>{
+  it("same folder", async ()=>{
 
-    return fs.writeFile('./testDir/template.txt', '{{name}}')
-      .then(()=>{
-        return cg.compile('./testDir/template.txt', {name: 'jhon'}, './testDir/res.txt' )
-      })
-      .then(()=>{
-        return fs.readFile('./testDir/res.txt');
-      }).then(f =>{
-        assert.isTrue(f == 'jhon')
-      });
+    await fs.writeFile('./testDir/template.txt', '{{name}}');
+    await cg.compile('./testDir/template.txt', {name: 'jhon'}, './testDir/res.txt');
+    let f = await fs.readFile('./testDir/res.txt');
+    assert.isTrue(f == 'jhon');
   })
 
-  it("preserves permissions", ()=>{
+  it("preserves permissions", async ()=>{
   
-    return fs.writeFile('./testDir/script.sh', '#!/bin/bash\necho {{name}}')
-      .then(()=>{
-        return fs.chmod('./testDir/script.sh', "755")
-      })
-      .then(()=>{
-        return cg.compile('./testDir/script.sh', {name: 'jhon'}, './testDir/script2.sh' )
-      })
-      .then(()=>{
-        return fs.stat('./testDir/script2.sh');
-      })
-      .then((s)=>{
-        assert.equal((s.mode & 0o7777).toString(8), "755");
-      });
-  
+    await fs.writeFile('./testDir/script.sh', '#!/bin/bash\necho {{name}}');
+    await fs.chmod('./testDir/script.sh', "755");
+    await cg.compile('./testDir/script.sh', {name: 'jhon'}, './testDir/script2.sh');
+    let f = await fs.stat('./testDir/script2.sh');
+    assert.equal((f.mode & 0o7777).toString(8), "755");
   });
 
-  it("bad inPath", ()=>{
-    return fs.writeFile('./testDir/template.txt', '{{name}}')
-      .then(()=>{
-        try{
-          return cg.compile({}, {name: 'jhon'}, './testDir/res.txt' )
-        }
-        catch(err){
-          assert.equal(err.message, "inPath must be a string")
-        }
-      });
+  it("bad inPath", async()=>{
+    await fs.writeFile('./testDir/template.txt', '{{name}}');
+    try{
+      await cg.compile({}, {name: 'jhon'}, './testDir/res.txt');
+      assert.fail()
+    }
+    catch(err){
+      assert.equal(err.message, "inPath must be a string")
+    }
   });
 
-  it("bad outPath", ()=>{
-    return fs.writeFile('./testDir/template.txt', '{{name}}')
-      .then(()=>{
-        try{
-          return cg.compile('./testDir/template.txt', {name: 'jhon'}, {} )
-        }
-        catch(err){
-          assert.equal(err.message, "outPath must be a string")
-        }
-      });
+  it("bad outPath", async()=>{
+    await fs.writeFile('./testDir/template.txt', '{{name}}');
+    try{
+      await cg.compile('./testDir/template.txt', {name: 'jhon'}, {} );
+      assert.fail()
+    }
+    catch(err){
+      assert.equal(err.message, "outPath must be a string")
+    }
   });
 
 });
 
 
 describe("copy conf", ()=>{
-  it("same folder", ()=>{
+  it("same folder", async ()=>{
 
-    return cgConf.copy('./f1.txt', './f1.txt')
-      .then(()=>{
-        return fs.stat('./outDir/f1.txt');
-      }).then(f =>{
-        assert.isTrue(f.isFile(), true);
-      });
+    await cgConf.copy('./f1.txt', './f1.txt');
+    let f = await fs.stat('./outDir/f1.txt');
+    assert.isTrue(f.isFile(), true);
   })
   
-  it("sub folder", ()=>{
-    return cgConf.copy('./f1.txt', './sub/f2.txt')
-      .then(()=>{
-        return fs.stat('./outDir/sub/f2.txt');
-      }).then(f =>{
-        assert.isTrue(f.isFile(), true);
-      });
+  it("sub folder", async ()=>{
+    await cgConf.copy('./f1.txt', './sub/f2.txt');
+    let f = await fs.stat('./outDir/sub/f2.txt'); 
+    assert.isTrue(f.isFile(), true);
   })
 
 
-  it("same folder no outPath", ()=>{
+  it("same folder no outPath", async ()=>{
 
-    return cgConf.copy('./f1.txt')
-      .then(()=>{
-        return fs.stat('./outDir/f1.txt');
-      }).then(f =>{
-        assert.isTrue(f.isFile(), true);
-      });
+    await cgConf.copy('./f1.txt');
+    let f = await fs.stat('./outDir/f1.txt');
+    assert.isTrue(f.isFile(), true);
   })
   
 });
 
 
 describe("copyDir conf", ()=>{
-  it("same folder", ()=>{
+  it("same folder", async ()=>{
 
-    return fs.mkdir('./testDir/dir1')
-      .then(()=>{
-        return fs.writeFile('./testDir/dir1/t1.txt', 'hello');
-      })
-      .then(()=>{
-        return cgConf.copyDir('./dir1', './dir2')
-      })
-      .then(()=>{
-        return fs.stat('./outDir/dir2/t1.txt');
-      }).then(f =>{
-        assert.isTrue(f.isFile(), true);
-      });
-  })
+    await fs.mkdir('./testDir/dir1');
+    await fs.writeFile('./testDir/dir1/t1.txt', 'hello');
+    await cgConf.copyDir('./dir1', './dir2');
+    let f = await fs.stat('./outDir/dir2/t1.txt');
+    assert.isTrue(f.isFile(), true);
+  });
   
-  it("same folder no outPath", ()=>{
+  it("same folder no outPath", async()=>{
 
-    return fs.mkdir('./testDir/dir1')
-      .then(()=>{
-        return fs.writeFile('./testDir/dir1/t1.txt', 'hello');
-      })
-      .then(()=>{
-        return cgConf.copyDir('./dir1')
-      })
-      .then(()=>{
-        return fs.stat('./outDir/dir1/t1.txt');
-      }).then(f =>{
-        assert.isTrue(f.isFile(), true);
-      });
+    await fs.mkdir('./testDir/dir1')
+    await fs.writeFile('./testDir/dir1/t1.txt', 'hello');
+    await cgConf.copyDir('./dir1');
+    let f = await fs.stat('./outDir/dir1/t1.txt');
+    assert.isTrue(f.isFile(), true);
   })
   
 });
 
 describe("compile conf", ()=>{
-  it("same folder", ()=>{
+  it("same folder", async ()=>{
 
-    return fs.writeFile('./testDir/template.txt', '{{name}}')
-      .then(()=>{
-        return cgConf.compile('./template.txt', {name: 'jhon'}, './testDir/res.txt' )
-      })
-      .then(()=>{
-        return fs.readFile('./outDir/testDir/res.txt');
-      }).then(f =>{
-        assert.isTrue(f == 'jhon')
-      });
+    await fs.writeFile('./testDir/template.txt', '{{name}}');
+    await cgConf.compile('./template.txt', {name: 'jhon'}, './testDir/res.txt' );
+    let f = await fs.readFile('./outDir/testDir/res.txt');
+    assert.isTrue(f == 'jhon');
   });
   
-  it("same folder no outPath", ()=>{
+  it("same folder no outPath", async ()=>{
 
-    return fs.mkdir('./testDir/subDir').
-      then(()=>{
-        return fs.writeFile('./testDir/subDir/template.txt', '{{name}}')
-      })
-      .then(()=>{
-        return cgConf.compile('./subDir/template.txt', {name: 'jhon'} )
-      })
-      .then(()=>{
-        return fs.readFile('./outDir/subDir/template.txt');
-      }).then(f =>{
-        assert.isTrue(f == 'jhon')
-      });
+    await fs.mkdir('./testDir/subDir');
+    await fs.writeFile('./testDir/subDir/template.txt', '{{name}}');
+    await cgConf.compile('./subDir/template.txt', {name: 'jhon'} );
+    let f = await fs.readFile('./outDir/subDir/template.txt');
+    assert.isTrue(f == 'jhon');
   });
 });
 
 
 describe("copyDir linux", ()=>{
-  it("same folder", ()=>{
+  it("same folder", async()=>{
 
-    return fs.mkdir('./testDir/dir1')
-      .then(()=>{
-        return fs.writeFile('./testDir/dir1/t1.txt', 'hello');
-      })
-      .then(()=>{
-        return cgConfLinux.copyDir('./dir1', './dir2')
-      })
-      .then(()=>{
-        return fs.stat('./outDir/dir2/t1.txt');
-      }).then(f =>{
-        assert.isTrue(f.isFile(), true);
-      });
-  })
+    await fs.mkdir('./testDir/dir1');
+    await fs.writeFile('./testDir/dir1/t1.txt', 'hello');
+    await cgConfLinux.copyDir('./dir1', './dir2');
+    let f = await fs.stat('./outDir/dir2/t1.txt');
+    assert.isTrue(f.isFile(), true);
+  });
   
-  it("same folder no outPath", ()=>{
+  it("same folder no outPath", async() => {
 
-    return fs.mkdir('./testDir/dir1')
-      .then(()=>{
-        return fs.writeFile('./testDir/dir1/t1.txt', 'hello');
-      })
-      .then(()=>{
-        return cgConfLinux.copyDir('./dir1')
-      })
-      .then(()=>{
-        return fs.stat('./outDir/dir1/t1.txt');
-      }).then(f =>{
-        assert.isTrue(f.isFile(), true);
-      });
+    await fs.mkdir('./testDir/dir1');
+    await fs.writeFile('./testDir/dir1/t1.txt', 'hello');
+    await cgConfLinux.copyDir('./dir1');
+    let f = await fs.stat('./outDir/dir1/t1.txt');
+    assert.isTrue(f.isFile(), true);
   })
   
 });
 
 describe("compile with helper", ()=>{
 
-  it("same folder", ()=>{
+  it("same folder", async ()=>{
 
-    return fs.writeFile('./testDir/template.txt', '{{loud name}}')
-      .then(()=>{
-        return cg.compile('./testDir/template.txt', {name: 'jhon'}, './testDir/res.txt' )
-      })
-      .then(()=>{
-        return fs.readFile('./testDir/res.txt');
-      }).then(f =>{
-        assert.isTrue(f == 'JHON');
-      });
+    await fs.writeFile('./testDir/template.txt', '{{loud name}}');
+    await cg.compile('./testDir/template.txt', {name: 'jhon'}, './testDir/res.txt' );
+    let f = await fs.readFile('./testDir/res.txt');
+    assert.isTrue(f == 'JHON');
   })
 
 });
